ae838e964ba28b567e7daf1f18db3309bcf7010d
PELLEGRIN Pierre <pellegrin.pierre@univ-catholyon.fr>
2020-02-18 15:01:19 +0100

    question yes/no sur la branch branche

--- script.py
+++ script.py
@@ -5,3 +5,11 @@ a = 2
 b = 12
 c = a + b
 print(c)
+print("Ca va ? ")
+valeur = str(input())
+if valeur == "y":
+    print("ok cool")
+elif valeur == "n":
+    print("qu'est-ce qui va pas bro ?")
+else:
+    print("réponds bro")
